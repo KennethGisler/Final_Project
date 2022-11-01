9f18422dad5d2258a600fe970055ce043f08fff5
KennethGisler <112647331+KennethGisler@users.noreply.github.com>
2022-11-01 17:20:45 -0500

    The start of our project

---
+++ JavaTwoFinal/src/main/java/ArmyBuilder/JavaTwoFinalApplication.java
@@ -0,0 +1,13 @@
+package ArmyBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTwoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTwoFinalApplication.class, args);
+	}
+
+}
---
+++ JavaTwoFinal/src/test/java/ArmyBuilder/JavaTwoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ArmyBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTwoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
